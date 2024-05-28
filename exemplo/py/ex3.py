78662e816d3b077e9af549fffe8e25f9812d2395
eygon.saldanha@dataprev.gov.br <eygon11@gmail.com>
2024-05-28 19:39:38 -0300

    EX3

---
+++ exemplo/py/ex3.py
@@ -0,0 +1,18 @@
+def home():
+    print('\n-----------------------\n')
+    print('Bem vindo')
+    print('1 - Ir para exemplo 1')
+    print('2 - Ir para exemplo 2')
+    print('3 - Sair')
+
+while True:
+    home()
+    op = int(input())
+    if op == 1:
+        import ex1
+    elif op == 2:
+        import ex2
+    elif op == 3:
+        break
+    else:
+        print('Opção inválida')
